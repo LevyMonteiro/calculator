037d722d83a1e2222e986a321ff9c6793bb6260a
Levy Monteiro <levymonteiro268@gmail.com>
2023-07-31 17:08:23 -0300

    create Display component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 import './styles/App.css'
 import { Keyboard } from './components/keyboard'
 import { calcData } from './context/calculatorData'
+import { Display } from './components/display'
 
 const App = () => {
 
   return (
     <div className='container'>
       <div className="calculator">
+        <Display />
         <Keyboard calcData={ calcData }/>
       </div>
     </div>
---
+++ src/components/display.jsx
@@ -0,0 +1,13 @@
+import '../styles/display.css'
+
+const Display = () => {
+  return (
+    <div className="display">
+      <h1>Calculator</h1>
+      <p className="history">History</p>
+      <h2 className="inputOutput">Input/Output</h2>
+    </div>
+  )
+}
+
+export { Display }
