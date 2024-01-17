3fedb23c839ea3007f773629b47cbc8450b19f63
Levy Monteiro <levymonteiro268@gmail.com>
2024-01-17 11:49:23 -0300

    install eslint-config-prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,86 @@
-import './styles/App.css'
-import { useState } from 'react'
-import { Keyboard } from './components/keyboard'
-import { Display } from './components/display'
-import { Footer } from './components/footer'
+import './styles/App.css';
+import { useState } from 'react';
+import { Keyboard } from './components/keyboard';
+import { Display } from './components/display';
+import { Footer } from './components/footer';
 
-const operators = ["AC", "/", "*", "+", "-", "="];
+const operators = ['AC', '/', '*', '+', '-', '='];
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const App = () => {
-
+function App() {
   const [input, setInput] = useState('0');
   const [output, setOutput] = useState('');
-  const [calcData, setCalcData] = useState('')
-  
+  const [calcData, setCalcData] = useState('');
+
   const handleInput = (value) => {
-     
     const handleSubmit = () => {
-      setOutput(`${calcData}${input}`)
-      setCalcData('')
-      const total = eval(`${calcData}${input}`)
-      setInput(total)
-    }
-    
+      setOutput(`${calcData}${input}`);
+      setCalcData('');
+      const total = eval(`${calcData}${input}`);
+      setInput(total);
+    };
+
     const handleClear = () => {
-      setInput('0')
-      setOutput('')
-      setCalcData('')
-    }
+      setInput('0');
+      setOutput('');
+      setCalcData('');
+    };
 
     const handleBackspace = () => {
-      if(input === '0' || input.length === 1) {
-        setInput('0')
-      } else if(input.length) {
-        setInput(input.slice(0, -1))
-        setCalcData(calcData.slice(0, -1))
+      if (input === '0' || input.length === 1) {
+        setInput('0');
+      } else if (input.length) {
+        setInput(input.slice(0, -1));
+        setCalcData(calcData.slice(0, -1));
       }
-    }
-    
-    const handleNumber = (value) => {
-      if(operators.some(op => op == input)) {
-        setCalcData(`${calcData}${input}`)
-        setInput(`${value}`)
-      } else {
-        if (input === '0' && value === 0) {
-          setInput('0')
-        } else if(input === '0' && value !== 0 ) {
-          setInput(`${value}`)
-        } else {
-          setInput(`${input}${value}`)
-        }
-      }
-    }
-          
-    const handleOperator = (value) => {
-      if(input !== '0' || calcData.length) {
-        if(operators.some(op => input === op)) {
-          setInput(value)
-        } else {
-          setCalcData(`${calcData}${input}`)
-          setInput(value)
-        }
-      } else if(input === '0') {
-        setInput('0')
-      } else {
-        setCalcData(`${calcData}${input}`)
-      } 
-    } 
-    
-    const handleDecimal = () => {
-      if(!calcData.length && input === '0') {
-        setInput('0.')
-      } else if(input.split('').some(elm => elm === '.')) {
-        setInput(input)
-      } else if(operators.find(op => op === input)) {
-        setCalcData(`${calcData}${input}`)
-        setInput('0.')
-      } else {
-        setInput(`${input}.`)
-      }
-    }
+    };
 
-  
-    const operator = operators.find(op => op === value)
-    const number = numbers.find(num => num === value)
+    const handleNumber = (value) => {
+      if (operators.some((op) => op == input)) {
+        setCalcData(`${calcData}${input}`);
+        setInput(`${value}`);
+      } else if (input === '0' && value === 0) {
+        setInput('0');
+      } else if (input === '0' && value !== 0) {
+        setInput(`${value}`);
+      } else {
+        setInput(`${input}${value}`);
+      }
+    };
+
+    const handleOperator = (value) => {
+      if (input !== '0' || calcData.length) {
+        if (operators.some((op) => input === op)) {
+          setInput(value);
+        } else {
+          setCalcData(`${calcData}${input}`);
+          setInput(value);
+        }
+      } else if (input === '0') {
+        setInput('0');
+      } else {
+        setCalcData(`${calcData}${input}`);
+      }
+    };
+
+    const handleDecimal = () => {
+      if (!calcData.length && input === '0') {
+        setInput('0.');
+      } else if (input.split('').some((elm) => elm === '.')) {
+        setInput(input);
+      } else if (operators.find((op) => op === input)) {
+        setCalcData(`${calcData}${input}`);
+        setInput('0.');
+      } else {
+        setInput(`${input}.`);
+      }
+    };
+
+    const operator = operators.find((op) => op === value);
+    const number = numbers.find((num) => num === value);
 
     switch (value) {
-      case '=': 
+      case '=':
         handleSubmit();
         break;
       case 'AC':
@@ -104,17 +99,17 @@ const App = () => {
         handleDecimal();
         break;
     }
-  }
+  };
 
   return (
     <div className='container'>
-      <div className="calculator">
-        <Display input={input} calcData={calcData} output={output}/>
-        <Keyboard handleInput={handleInput}/>
+      <div className='calculator'>
+        <Display input={input} calcData={calcData} output={output} />
+        <Keyboard handleInput={handleInput} />
       </div>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
